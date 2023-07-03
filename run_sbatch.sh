c07e796a083c72f972d542a6343ba11ab1ae3b17
SHA-T <tel.shamoyan@gmail.com>
2023-07-04 01:23:05 +0200

    Added hyperparameters.json & improved sbatchscript

--- run_sbatch.sh
+++ run_sbatch.sh
@@ -2,44 +2,41 @@
 
 #SBATCH --mail-user=t.shamoyan@stud.uni-hannover.de
 #SBATCH --mail-type=BEGIN,END
-#SBATCH --job-name=ion_channel_si
+#SBATCH --job-name=enzyme_with_indications
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
-#SBATCH --time=1:00:00
+#SBATCH --mem-per-gpu=16G
+#SBATCH --time=02:00:00
 #SBATCH --output=%x.%j.out.log
 #SBATCH --error=%x.%j.err.log
 
 # ---------- Parameters ----------
-DATASET_ROOT_PATH="data_k_fold/yamanishi/ion_channel"
-DATASET_NAME="with_similarity_information_top0.05pct"
-FOLD="1"
+DATASET_ROOT="data_k_fold/yamanishi"
+DATASET_TYPE="enzyme"
+DATASET_NAME="with_indications"
+FOLD=$SLURM_ARRAY_TASK_ID
+DATA_PATH="${DATASET_ROOT}/${DATASET_TYPE}/${DATASET_NAME}/${FOLD}"
 
-MODEL_KGE="ComplEx"                # TransE | RotatE | ComplEx | DistMult
+MODEL_KGE="TransE"                # TransE | RotatE | ComplEx | DistMult
 
-NEGATIVES=32
-BATCHSIZE=256
-DIM=50
-GAMMA=3.575
-LR=0.0199
-MAX_STEPS=2500
+HP=`apptainer exec /nfs/data/env/jq.sif jq ".${DATASET_TYPE}.${MODEL_KGE}" hyperparameters.json`
+
+NEGATIVES=$(echo $HP | apptainer exec /nfs/data/env/jq.sif jq ".NEGATIVES" -r)
+BATCHSIZE=$(echo $HP | apptainer exec /nfs/data/env/jq.sif jq ".BATCHSIZE" -r)
+DIM=$(echo $HP | apptainer exec /nfs/data/env/jq.sif jq ".DIM" -r)
+GAMMA=$(echo $HP | apptainer exec /nfs/data/env/jq.sif jq ".GAMMA" -r)
+LR=$(echo $HP | apptainer exec /nfs/data/env/jq.sif jq ".LR" -r)
+MAX_STEPS=$(echo $HP | apptainer exec /nfs/data/env/jq.sif jq ".MAX_STEPS" -r)
 VAL_STEPS=$(expr $MAX_STEPS / 20)
+DE=$(echo $HP | apptainer exec /nfs/data/env/jq.sif jq ".DE" -r)
+DR=$(echo $HP | apptainer exec /nfs/data/env/jq.sif jq ".DR" -r)
+REG=$(echo $HP | apptainer exec /nfs/data/env/jq.sif jq ".REG" -r)
 
-DE=""
-DR=""
-REG=""
-if [ "$MODEL_KGE" = "RotatE" ]; then
-  DE="-de"
-fi
-if [ "$MODEL_KGE" = "ComplEx" ]; then
-  DE="-de"
-  DR="-dr"
-  REG="-r 0.001"
-fi
-
-NEG_SAMPLING_METHOD="jaccard"     # uniform | jaccard | count
+NEG_SAMPLING_METHOD="uniform"     # uniform | jaccard | count
 
 WANDB_ENTITY="l3s-future-lab"
-WANDB_PROJECT="test_yamanishi_si"
+WANDB_PROJECT="${DATASET_TYPE}_${DATASET_NAME}"
+WANDB_RUN_NAME="${MODEL_KGE}_fold${FOLD}"
 
 # --------- Load Modules ---------
 source ~/.zshrc
@@ -51,18 +48,20 @@ export WANDB_API_KEY=$wandb_api_key
 
 python -u codes/run.py \
  --wandb_project $WANDB_PROJECT \
+ --wandb_entity $WANDB_ENTITY \
+ --wandb_run_name $WANDB_RUN_NAME \
  --neg_sampling_method $NEG_SAMPLING_METHOD \
  --do_train \
- --do_pretrain \
  --cuda \
  --seed 42 \
  --do_valid \
  --do_test \
- --data_path "${DATASET_ROOT_PATH}/${DATASET_NAME}/${FOLD}" \
+ --data_path $DATA_PATH \
  --model $MODEL_KGE \
  --valid_steps $VAL_STEPS \
  --log_steps $VAL_STEPS \
  -n $NEGATIVES -b $BATCHSIZE -d $DIM \
  -g $GAMMA -a 1.0 -adv \
  -lr $LR --max_steps $MAX_STEPS \
- -save models/"${DATASET_NAME}_${MODEL_KGE}" --test_batch_size 8 $DE $DR $REG
+ -save models/"${DATASET_TYPE}_${DATASET_NAME}_${MODEL_KGE}" \
+ --test_batch_size 8 $DE $DR $REG
